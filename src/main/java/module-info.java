1120dc55aeaa16c50bfb201fadae72ef15e919c9
pabeler <pabeler@o2.pl>
2023-01-03 18:06:32 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.demo1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.demo1 to javafx.fxml;
+    exports com.example.demo1;
+}

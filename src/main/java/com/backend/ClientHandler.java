b36969176cfa29c82a901684c1384612844ba1df
pabeler <pabeler@o2.pl>
2023-01-10 16:20:41 +0100

    Add documentation for ClientHandler class

--- src/main/java/com/backend/ClientHandler.java
+++ src/main/java/com/backend/ClientHandler.java
@@ -13,7 +13,6 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import java.io.EOFException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -28,37 +27,71 @@ import java.util.regex.Pattern;
  * Class that handles the client's requests.
  */
 public class ClientHandler extends Thread {
-    private Socket s;
-    private ObjectInputStream dis;
-    private ObjectOutputStream dos;
+    /**
+     * Service registry used to create the session factory.
+     */
+    final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure("hibernate.cfg.xml")
+            .build();
+    /**
+     * Session used to communicate with the database.
+     */
+    Session session;
+    /**
+     * Object DataPackage used to store data to send or receive from client.
+     */
+    DataPackage dataPackage;
+    /**
+     * Map dataPackageMap used to store data in HashMap form.
+     */
+    HashMap<String, Object> dataPackageMap;
+    /**
+     * Socket used to communicate with the client.
+     */
+    private final Socket s;
+    /**
+     * Object output stream used to send data to the client.
+     */
+    private final ObjectInputStream dis;
+    /**
+     * Object input stream used to receive data from the client.
+     */
+    private final ObjectOutputStream dos;
+    /**
+     * Variable used to check if the client is still connected.
+     */
     private boolean isRunning = true;
+    /**
+     * Session factory used to create sessions.
+     */
     private SessionFactory sessionFactory;
 
+    /**
+     * Constructor that creates a client handler.
+     *
+     * @param s socket used to communicate with the client, dis object input stream used to receive data from the client,
+     *          dos object output stream used to send data to the client
+     */
     public ClientHandler(Socket s, ObjectInputStream dis, ObjectOutputStream dos) {
         this.s = s;
         this.dis = dis;
         this.dos = dos;
     }
 
+    /**
+     * Method that handles the client's requests.
+     */
     public void run() {
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure("hibernate.cfg.xml") // configures settings from hibernate.cfg.xml
-                .build();
         try {
             sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
         } catch (Exception e) {
-            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
-            // so destroy it manually.
             StandardServiceRegistryBuilder.destroy(registry);
         }
-        Session session = sessionFactory.openSession();
+        session = sessionFactory.openSession();
         while (isRunning) {
             try {
-                // Ask user what he wants
-                DataPackage dataPackage = (DataPackage) dis.readObject();
-                HashMap<String, Object> data = dataPackage.getData();
-                // write on output stream based on the
-                // answer from the client
+                dataPackage = (DataPackage) dis.readObject();
+                dataPackageMap = dataPackage.getData();
                 switch (dataPackage.getCommand()) {
                     case EXIT_PROGRAM -> {
                         System.out.println("Client " + this.s + " sends exit...");
@@ -69,12 +102,13 @@ public class ClientHandler extends Thread {
                     }
                     case ADD_STUDENT -> {
                         try {
-                            if (((String) data.get("Imie")).isEmpty() || ((String) data.get("Nazwisko")).isEmpty()) {
+                            if (((String) dataPackageMap.get("Imie")).isEmpty() ||
+                                    ((String) dataPackageMap.get("Nazwisko")).isEmpty()) {
                                 throw new Exception("Imie i nazwisko nie moga byc puste");
                             }
                             Student student = new Student();
-                            student.setImie((String) data.get("Imie"));
-                            student.setNazwisko((String) data.get("Nazwisko"));
+                            student.setImie((String) dataPackageMap.get("Imie"));
+                            student.setNazwisko((String) dataPackageMap.get("Nazwisko"));
                             student.setGrupa(null);
                             session.getTransaction().begin();
                             session.persist(student);
@@ -86,7 +120,7 @@ public class ClientHandler extends Thread {
                     }
                     case DELETE_STUDENT -> {
                         try {
-                            Student student = session.find(Student.class, data.get("ID_Studenta"));
+                            Student student = session.find(Student.class, dataPackageMap.get("ID_Studenta"));
                             if (student == null) {
                                 throw new Exception("Nie ma takiego studenta");
                             }
@@ -100,11 +134,11 @@ public class ClientHandler extends Thread {
                     }
                     case ADD_GROUP -> {
                         try {
-                            if (((String) data.get("Nazwa")).isEmpty()) {
+                            if (((String) dataPackageMap.get("Nazwa")).isEmpty()) {
                                 throw new Exception("Nazwa grupy nie moze byc pusta");
                             }
                             Grupa grupa = new Grupa();
-                            grupa.setNazwa((String) data.get("Nazwa"));
+                            grupa.setNazwa((String) dataPackageMap.get("Nazwa"));
                             grupa.setTermin(null);
                             session.getTransaction().begin();
                             session.persist(grupa);
@@ -116,7 +150,7 @@ public class ClientHandler extends Thread {
                     }
                     case DELETE_GROUP -> {
                         try {
-                            Grupa grupa = session.find(Grupa.class, data.get("ID_Grupy"));
+                            Grupa grupa = session.find(Grupa.class, dataPackageMap.get("ID_Grupy"));
                             if (grupa == null) {
                                 throw new Exception("Nie ma takiej grupy");
                             }
@@ -130,16 +164,16 @@ public class ClientHandler extends Thread {
                     }
                     case ADD_STUDENT_TO_GROUP -> {
                         try {
-                            if (session.find(Grupa.class, data.get("ID_Grupy")) == null) {
+                            if (session.find(Grupa.class, dataPackageMap.get("ID_Grupy")) == null) {
                                 throw new Exception("Nie ma takiej grupy");
                             }
-                            Student student = session.find(Student.class, data.get("ID_Studenta"));
+                            Student student = session.find(Student.class, dataPackageMap.get("ID_Studenta"));
                             if (student == null) {
                                 throw new Exception("Nie ma takiego studenta");
                             }
                             session.getTransaction().begin();
                             session.detach(student);
-                            student.setGrupa((int) data.get("ID_Grupy"));
+                            student.setGrupa((int) dataPackageMap.get("ID_Grupy"));
                             session.merge(student);
                             session.getTransaction().commit();
                             dos.writeObject(new DataPackage(DataPackage.Command.SUCCESSFUL, null));
@@ -149,7 +183,7 @@ public class ClientHandler extends Thread {
                     }
                     case REMOVE_STUDENT_FROM_GROUP -> {
                         try {
-                            Student student = session.find(Student.class, data.get("ID_Studenta"));
+                            Student student = session.find(Student.class, dataPackageMap.get("ID_Studenta"));
                             if (student == null) {
                                 throw new Exception("Nie ma takiego studenta");
                             }
@@ -170,15 +204,15 @@ public class ClientHandler extends Thread {
                                             + "|^(((19|2[0-9])[0-9]{2})-02-(0[1-9]|1[0-9]|2[0-8]))$"
                                             + "|^(((19|2[0-9])[0-9]{2})-(0[13578]|10|12)-(0[1-9]|[12][0-9]|3[01]))$"
                                             + "|^(((19|2[0-9])[0-9]{2})-(0[469]|11)-(0[1-9]|[12][0-9]|30))$");
-                            Matcher matcher = DATE_PATTERN.matcher((String) data.get("Data"));
+                            Matcher matcher = DATE_PATTERN.matcher((String) dataPackageMap.get("Data"));
                             if (!matcher.matches()) {
                                 throw new Exception("Niepoprawna data");
                             }
-                            Grupa grupa = session.find(Grupa.class, data.get("ID_Grupy"));
+                            Grupa grupa = session.find(Grupa.class, dataPackageMap.get("ID_Grupy"));
                             if (grupa == null) {
                                 throw new Exception("Nie ma takiej grupy");
                             }
-                            grupa.setTermin(Date.valueOf((String) data.get("Data")));
+                            grupa.setTermin(Date.valueOf((String) dataPackageMap.get("Data")));
                             session.getTransaction().begin();
                             session.detach(grupa);
                             session.merge(grupa);
@@ -190,16 +224,16 @@ public class ClientHandler extends Thread {
                     }
                     case CHECK_ABSENCE -> {
                         try {
-                            if (data.get("Obecnosc") == null) {
+                            if (dataPackageMap.get("Obecnosc") == null) {
                                 throw new Exception("Obecnosc nie moze byc pusta");
                             }
-                            Student student = session.find(Student.class, data.get("ID_Studenta"));
+                            Student student = session.find(Student.class, dataPackageMap.get("ID_Studenta"));
                             if (student == null) {
                                 throw new Exception("Nie ma takiego studenta");
                             }
                             session.getTransaction().begin();
                             session.detach(student);
-                            student.setObecnosc((String) data.get("Obecnosc"));
+                            student.setObecnosc((String) dataPackageMap.get("Obecnosc"));
                             session.merge(student);
                             session.getTransaction().commit();
                             dos.writeObject(new DataPackage(DataPackage.Command.SUCCESSFUL, null));
@@ -247,7 +281,6 @@ public class ClientHandler extends Thread {
             }
         }
         try {
-            // closing resources
             this.dis.close();
             this.dos.close();
         } catch (IOException e) {
@@ -255,6 +288,11 @@ public class ClientHandler extends Thread {
         }
     }
 
+    /**
+     * Getter for Socket object
+     *
+     * @return the socket associated with the connection.
+     */
     public Socket getSocket() {
         return s;
     }
